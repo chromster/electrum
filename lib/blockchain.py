675af87cd22c3bb96d8caa344dbe6d270841ab7b
chromster <32965831+chromster@users.noreply.github.com>
2017-11-03 21:00:32 +0100

    Update blockchain.py

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -312,9 +312,9 @@ class Blockchain(util.PrintError):
         bits, target = self.get_target(height // 2016)
         try:
             #self.verify_header(header, previous_header, bits, target)
-        #except:
+        except:
             #return False
-            return True
+        return True
 
     def connect_chunk(self, idx, hexdata):
         try:
